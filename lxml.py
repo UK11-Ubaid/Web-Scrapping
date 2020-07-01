d8eea1533d7c7ee925a10ac55f04d7ba015b8d97
UK11-Ubaid <58435969+UK11-Ubaid@users.noreply.github.com>
2020-07-01 19:24:56 +0200

    Update lxml.py

--- lxml.py
+++ lxml.py
@@ -8,8 +8,20 @@ tree = html.fromstring(page.content)
 catchphrase = tree.xpath('//p[@class = "catchphrase"]/text()')
 amount = tree.xpath('//span[@class = "amountRange"]/text()')
 repPeriod = tree.xpath('//span[@class = "repaymentPeriod"]/text()')
-print(catchphrase)
-print(amount)
-print(repPeriod)
+#from line 11 to 14 will print each element and then go to next to print
+#print(catchphrase)
+#print(amount)
+#print(repPeriod)
+
+#line 16 to 25 will print info in a group
+output = []
+for info in zip(catchphrase,amount, repPeriod):
+    resp = {}
+    resp['catchphrase'] = info[0]
+    resp['amount'] = info[1]
+    resp['period'] = info[2]
+    output.append(resp)
+
+print(output)
 
 
