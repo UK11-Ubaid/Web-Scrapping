9919870a9a9b7fdabb79b60560049c5d1b594617
UK11-Ubaid <58435969+UK11-Ubaid@users.noreply.github.com>
2020-07-03 15:00:42 +0200

    Update lxml.py

--- lxml.py
+++ lxml.py
@@ -1,3 +1,4 @@
+#imports
 from lxml import html
 import requests
 import pandas as pd
@@ -29,20 +30,13 @@ p = div.getchildren()[4].getchildren()[0].getchildren()[-1]
 links = obj.xpath('/html/body/main/div/div/div[1]/div[2]/article[*]/p[3]/a[1]')
 links = [l.attrib['href'] for l in links]
 
-
-#getting error at index 2 because it does not have benefits so its product offered is at index 6 and not 7
-lnk = len(links)
-for l in range(lnk):
-    r = session.get(links[l])
-    absa = lxml.html.document_fromstring(r.text)  
-    section = absa.xpath('/html/body/main/div/div/div[1]/section[2]')
-    sec = section[0].getchildren()[7].getchildren()
-    for value in sec:
-        print(value.text)
-
-
-
-     
+r = session.get(links[11])
+absa = lxml.html.document_fromstring(r.text)  
+section = absa.xpath('/html/body/main/div/div/div[1]/section[2]')
+paragraphs = section[0].getchildren()
+sec = paragraphs[-1].getchildren() 
+  
+    
 print("Getting suppliers info...")
 time.sleep(1)
 supplier = []
@@ -50,33 +44,50 @@ for i in links:
     url_parts = urllib.parse.urlparse(i)
     path_parts = url_parts[2].rpartition('/')[2]
     supplier.append(path_parts)
-    
-    
+
 print("Getting catchphrase...")   
 time.sleep(1)
 print("Getting amount...")
 time.sleep(1)
 print("Getting repayment period...")
 time.sleep(1)
+
+print("Getting products offered...")
+time.sleep(1)
+lnk = len(links)
+offer = []
+for l in range(lnk):
+        r = session.get(links[l])
+        banks = lxml.html.document_fromstring(r.text)  
+        section = banks.xpath('/html/body/main/div/div/div[1]/section[2]')
+        paragraphs = section[0].getchildren()
+        sec = paragraphs[-1].getchildren() 
+        bs = []
+        for value in sec:
+            bs.append(value.text)
+        offer.append(', '.join(bs))
+
+        
 print("Getting links...")
 time.sleep(1)
 
 print("Creating a table...")
 time.sleep(2)
-
 Supplier = []
 CatchPhrase = []
 Amount = []
 Period = []
 Product = []
 Link = []
-for info in zip(supplier,catchphrase, amount,repPeriod, links):
+for info in zip(supplier,catchphrase, amount,repPeriod, offer, links):
     Supplier.append(info[0])
     CatchPhrase.append(info[1])
     Amount.append(info[2])
     Period.append(info[3])
-    Link.append(info[4])
+    Product.append(info[4])
+    Link.append(info[5])
+     
     
-df = pd.DataFrame({'Supplier': Supplier, 'Catchphrase': CatchPhrase, 'Amount': Amount, 'RepaymentPeriod': Period, 'Links': Link})
+df = pd.DataFrame({'Supplier': Supplier, 'Catchphrase': CatchPhrase, 'Amount': Amount, 'RepaymentPeriod': Period, 'Products Offered': offer, 'Links': Link})
 df
 
